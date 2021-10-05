171adee5c74a9940a168c56999f23801e7b95e77
Ahmad Zeb <ahmadzebmcs@outlook.com>
2021-10-05 13:11:39 -0700

    ahmad changes

--- Daawahla.WebApi/Startup.cs
+++ Daawahla.WebApi/Startup.cs
@@ -28,7 +28,7 @@ namespace Daawahla.WebApi
         }
         //ahmad zeb test
         public IConfiguration Configuration { get; }
-
+        //Hi ahmad
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
